143f278c50fb6960ce85a55148254a5aa576d878
MelnykIhor <melnyk70@gmail.com>
2024-10-23 19:46:27 +0300

    DZ_9_1

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# ДЗ 9.1. Визначити популярність певних слів у тексті
+# На вхід функції popular_words передаються два аргументи. Текст та список слів,
+# популярність яких необхідно визначити.
+# При вирішенні цього завдання зверніть увагу на такі моменти
+# Слова необхідно шукати у всіх регістрах. Тобто. якщо необхідно знайти
+# слово "one", значить для нього будуть підходити слова "one", "One", "oNe", "ONE" і т.д.
+# Шукані слова завжди вказані в нижньому регістрі
+# Якщо слово не знайдено жодного разу, його необхідно повернути у словнику зі значенням 0 (нуль)
+# Вхідні параметри: Текст і масив слів, що шукаються.
+# Вихідні параметри: Словник, у якому ключами є шукані слова та значеннями,
+# скільки разів кожнє слово зустрічаються у орігінальному тексті.
+# Приклад:
+# def popular_words (text, words):
+# pass
+# assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1' print('OK')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
