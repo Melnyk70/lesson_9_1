74e41d418531ae9d2260c8d18649f3d1c4784097
MelnykIhor <melnyk70@gmail.com>
2024-10-23 21:30:56 +0300

    DZ_9_1

--- main.py
+++ main.py
@@ -25,6 +25,5 @@ def popular_words (text, words):
        my_dict.update({words[i]: ll})
        i=i+1
    return my_dict
-
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
 print('OK')
